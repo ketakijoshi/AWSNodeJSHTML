1b8f8d2ed225c91b16f877f189763925d9105538
Ketaki Joshi <ketaki.joshi@shorelineiot.com>
2016-12-22 16:21:07 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+//var ipAddress = "127.0.0.1";
+
+var portNumber = process.env.PORT || 3000,
+    httpModule = require("http");
+
+httpModule.createServer(
+    function serviceRequest (request, response) {
+
+      var queryString = new String(request.url);
+
+      var keyValuePairs = queryString.split("&"); 
+
+      var action = keyValuePairs[0].replace("/","").split("=")[1]; 
+      var firstNumber = new String(keyValuePairs[1].split("&")).split("=")[1] || "0"; 
+      var secondNumber = new String(keyValuePairs[2].split("&")).split("=")[1] || "0"; 
+      var result = getResult(action.toLowerCase(), Number(firstNumber) , Number(secondNumber));
+
+      
+
+
+
+
+      var htmlContent = "<html><b>" + action + "(" + firstNumber + "," + secondNumber + ") = <b>" + result + "</b></html>";
+      response.end(htmlContent);
+    }
+).listen(portNumber);
+
+
+function getResult(operation, number1, number2)
+{
+  var result = 0;
+
+  if(operation == "add")
+    result = number1 + number2;
+
+  else if(operation == "subtract")
+    result = number1 - number2;
+
+  else if(operation == "multiply")
+    result = number1 * number2;
+
+  else if(operation == "divide" && number2 != 0)
+    result = number1 / number2;
+
+  return result;
+}
+
